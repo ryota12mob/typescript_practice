1423b848900caef36405179599806490450b7976
熊坂亮汰 <kuma@kumasakaryounoMacBook-Pro.local>
2022-07-30 19:21:22 +0900

    number型とstring型、型の定義

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('ここにコードを追加します!');
+function add(n1, n2, showResult, phrase) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('入力値が正しくありません');
+    // }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result';
+add(number1, number2, printResult, resultPhrase);
